d6068bc81665e8946cb63ec852ed6c8a13c405f1
vanyaruda <ivan.ruda98@gmail.com>
2023-01-15 16:59:21 +0200

    add HW 3,4,5.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
-let name = prompt('What is your name?');
+// let name = prompt('What is your name?');
 
-alert(`Hello, ${name}! How are you?`);
+// alert(`Hello, ${name}! How are you?`);
+
+// ! ДЗ 3. Математичні операції
+
+let userFirstNumber = +prompt('the first number');
+let userSecondNumber = +prompt('the second number');
+alert(userFirstNumber + userSecondNumber);
+alert(userFirstNumber - userSecondNumber);
+alert(userFirstNumber * userSecondNumber);
+alert(userFirstNumber / userSecondNumber);
+
+// ! ДЗ 4. Години в секунди
+
+let userTime = +prompt('What time is it?')
+alert(`${userTime * 60 ** 2} seconds`);
+
+// ! ДЗ 5. Середнє значення
+
+let userFirstMediumNumber = +prompt('first number');
+let userSecondMediumNumber = +prompt('second nimber');
+let userThirdMediumNumber = +prompt('third number');
+alert((userFirstMediumNumber + userSecondMediumNumber + userThirdMediumNumber) / 3);
