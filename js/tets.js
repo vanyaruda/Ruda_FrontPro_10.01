5a4ca7a33f8e24ea6f6efa9ac2175f3dbf354896
vanyaruda <ivan.ruda98@gmail.com>
2023-02-03 16:48:56 +0200

    add HW16

---
+++ js/tets.js
@@ -0,0 +1,15 @@
+const UpperCase = ("background-color")
+function camelize(str) {
+    return str
+        .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+        .map(
+            // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+            // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+            (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+        )
+        .join('');
+    // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+}
+console.log(camelize(UpperCase))
+const blabla = ("list-style-image")
+console.log(camelize(blabla))
