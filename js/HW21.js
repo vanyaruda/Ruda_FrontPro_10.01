29f91652abf17b44c269ecb68c74560b87e77789
vanyaruda <ivan.ruda98@gmail.com>
2023-02-12 14:57:25 +0200

    add HW21

---
+++ js/HW21.js
@@ -0,0 +1,17 @@
+// !ДЗ 21. this chain
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this
+    },
+    down: function () {
+        this.step--;
+        return this
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+        return this
+    }
+};
+console.log(ladder.up().up().down().showStep()) 
