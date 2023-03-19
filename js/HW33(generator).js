7c8c29ace237b41bc8f9d3ee11e8522b57c14639
vanyaruda <ivan.ruda98@gmail.com>
2023-03-19 12:20:27 +0200

    corrHW

--- js/HW33(generator).js
+++ js/HW33(generator).js
@@ -19,40 +19,50 @@ for (let res of generator) {
 
 
 // 2) Создать свой распорядок дня с помощью async await ( тоесть мы должны "подождать" выполнение асинхронной функции brushTeeth перед тем как приступать к выполнению следующей асинхронной функции
-
+const delay = ms => {
+    return new Promise((r) => setTimeout(() => r(), ms))
+}
 
 
 const morning = new Promise((resolve) => {
-    setTimeout(() => resolve('I wake up at 7 o\'clock'), 1000)
+    resolve('I wake up at 7 o\'clock')
 
 });
 const breakfast = new Promise((resolve) => {
-    setTimeout(() => resolve('I have breakfest at 8 o\'clock'), 2000)
+    resolve('I have breakfest at 8 o\'clock')
 
 });
 const lunch = new Promise((resolve) => {
-    setTimeout(() => resolve(`I have lunch at 12 o\'clock`), 3000)
+    resolve(`I have lunch at 12 o\'clock`)
 })
 const cameHome = new Promise((resolve) => {
-    setTimeout(() => resolve(`I came home at 19 o\'clock`), 4000)
+    resolve(`I came home at 19 o\'clock`)
 });
 const goSleep = new Promise((resolve) => {
-    setTimeout(() => resolve(`I go to sleep at 23:00 o\'clock`), 5000)
+    resolve(`I go to sleep at 23:00 o\'clock`)
 })
 
 
 const myDay = async () => {
-    const morningRes = await morning
-    console.log(morningRes);
-    const breakfastRes = await breakfast
-    console.log(breakfastRes);
-    const lunchRes = await lunch
-    console.log(lunchRes);
-    const cameHomeRes = await cameHome
-    console.log(cameHomeRes);
-    const sleep = await goSleep
-    console.log(sleep);
-
+    try {
+        await delay(1500)
+        const morningRes = await morning
+        console.log(morningRes);
+        await delay(2500)
+        const breakfastRes = await breakfast
+        console.log(breakfastRes);
+        await delay(3000)
+        const lunchRes = await lunch
+        console.log(lunchRes);
+        await delay(4000)
+        const cameHomeRes = await cameHome
+        console.log(cameHomeRes);
+        await delay(5000)
+        const sleep = await goSleep
+        console.log(sleep);
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 myDay()
