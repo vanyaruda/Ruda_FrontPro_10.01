5d089fe3e75baeb16447b78b6a5704ea24c5fedb
vanyaruda <ivan.ruda98@gmail.com>
2023-04-06 09:58:05 +0300

    create HW37

---
+++ HW37/script.js
@@ -0,0 +1,9 @@
+const body = document.body
+const hwTitle = document.createElement('p')
+
+body.style.backgroundColor = 'bisque'
+hwTitle.innerHTML = 'ДЗ 37. Перехід за посиланням'
+hwTitle.style.fontFamily = 'Times New Roman'
+hwTitle.style.marginBottom = '30px'
+hwTitle.style.fontSize = '30px'
+body.prepend(hwTitle)
